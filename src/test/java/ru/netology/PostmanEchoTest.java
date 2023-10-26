d5b3d4f4cd7ef9b16d329e2e012bcc34f35f852e
vysavely <s01g@ya.ru>
2023-10-26 23:40:44 +0300

    Error fixed

--- src/test/java/ru/netology/PostmanEchoTest.java
+++ src/test/java/ru/netology/PostmanEchoTest.java
@@ -20,7 +20,7 @@ public class PostmanEchoTest {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("TestTest"))
+                .body("data", equalTo("TestTestTest"))
         ;
 
     }
